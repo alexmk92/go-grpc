3f709b93060ed0315b9eae5fffb26b63f923b9d7
Alexander Sims <alexander.sims92@gmail.com>
2021-05-18 18:30:59 +0100

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "log"
+)
+
+func Run() error {
+    // responsible for init and starting gRPC server
+    return nil
+}
+
+func main () {
+    if err := Run(); err != nil {
+        log.Fatal(err)
+    }
+}
---
+++ internal/product/product.go
@@ -0,0 +1,60 @@
+//go:generate mockgen -destination=product_mocks_test.go -package=product -package=product github.com/alexmk92/go-grpc/internal/product Store
+package product
+
+
+import "context"
+
+type Product struct {
+    ID string
+    Name string
+    Type string
+    Orders int
+}
+
+// Defines the interface we expect our db to conform to
+type Store interface {
+    GetProductById(id string) (Product, error)
+    InsertProduct(product Product) (Product, error)
+    DeleteProduct(id string) error
+}
+
+// Product service responsible for updating
+// the product db
+type Service struct {
+    Store Store
+}
+
+// Returns a new product service
+func New(store Store) Service {
+    return Service{
+        Store: store
+    }
+}
+
+// Retrieves a product based on the ID
+func (s Service) GetProductById(ctx context.Context, id string) (Product, error) {
+    product, err := s.Store.GetProductById(id)
+    if err != nill {
+        return Product{}, err
+    }
+
+    return product, nil
+}
+
+// Inserts a new product
+func (s Service) InsertProduct(ctx context.Context, product Product) (Product, error) {
+    product, err := s.Store.InsertProduct(product)
+    if err != nil {
+        return Product{}, err
+    }
+    return product, nil
+}
+
+// Delete a product
+func (s Service) Delete(ctx context.Context, id string) error {
+    err := s.Store.DeleteProduct(id)
+    if err != nil {
+        return err
+    }
+    return nil
+}
